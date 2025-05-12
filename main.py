61fe72e6a5f348f6a8af534b619260d2cfd26cae
Micah Pearce <micah.r.pearce@gmail.com>
2025-05-11 23:31:37 -0500

    add streamlit code

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+# turorial: error: https://github.com/data-sloth/uv-streamlit-setup/
+import streamlit as st
+from myproject.pkg2 import complex_fn
+
 def main():
-    print("Hello from uv-streamlit!")
+    st.header("Hello from uv-setup-example!")
+
+    val1 = st.number_input('Value 1')
+    val2 = st.number_input('Value 2')
+    val3 = st.number_input('Value 3')
+
+    st.write(f"The square of ({val1} - {val2}) is {complex_fn.square_of_diff(val1, val2)}!")
+    st.write(f"The square of {val3} is {complex_fn.square_of_diff(val3,  0)}!")
 
 
 if __name__ == "__main__":
